1cb973268a2a71d59f1c17c9cbcc6b5a73c795dd
Pranav Garg <64488260+pranavgarg039@users.noreply.github.com>
2020-12-13 19:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,27 @@ import pandas as pd
 import streamlit as st
 from sklearn.ensemble import GradientBoostingRegressor
 
-
-st.write("""
+def main():
+         st.write("""
          #Distance Predictor
-""")
+         """)
 
-def inp_features():
-    Heartbeat = st.sidebar.slider('heartbeat', 50,190,100)
-    Cadence = st.sidebar.slider('Cadence', 100,250, 130)
-    Speed = st.sidebar.slider('Speed', 2,36,10)
-    
-    inp = [Heartbeat, Cadence, Speed]
-    return inp
+         def inp_features():
+                  Heartbeat = st.sidebar.slider('heartbeat', 50,190,100)
+                  Cadence = st.sidebar.slider('Cadence', 100,250, 130)
+                  Speed = st.sidebar.slider('Speed', 2,36,10)
 
-df = inp_features()
-st.write(df)
-model = pickle.load(open('model.pkl', 'rb'))
-ans = model.predict([df])
-st.write(ans)
+                  inp = [Heartbeat, Cadence, Speed]
+                  return inp
+
+         df = inp_features()
+         st.write(df)
+         model = pickle.load(open('model.pkl', 'rb'))
+         ans = model.predict([df])
+         st.write(ans)
+
+if __name__== "__main__":
+         main()
 
         
     
